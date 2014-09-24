0541b294a077beb1c575be8e1cd0ae3c980fdf2d
kongtong.ouyang <kongtong.ouyang>
2014-09-24 23:25:34 +0800

    init

---
+++ src/main/java/com/owz/furry/A.java
@@ -0,0 +1,8 @@
+package com.owz.furry;
+
+/**
+ * @version $Id$
+ * @auther ouyangkongtong Created on 2014/9/24, 22:03
+ */
+public class A {
+}
---
+++ src/test/java/com/owz/furry/SpringTest.java
@@ -0,0 +1,8 @@
+package com.owz.furry;
+
+/**
+ * @version $Id$
+ * @auther ouyangkongtong Created on 2014/9/24, 22:13
+ */
+public class SpringTest {
+}
