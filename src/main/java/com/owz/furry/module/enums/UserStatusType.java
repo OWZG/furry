213f3a83513055f55a34140a78a429563e16fa18
kongtong.ouyang <kongtong.ouyang@qunar.com>
2014-09-25 19:12:37 +0800

    initial commit

--- src/main/java/com/owz/furry/module/enums/UserStatusType.java
+++ src/main/java/com/owz/furry/module/enums/UserStatusType.java
@@ -3,11 +3,18 @@
 */
 package com.owz.furry.module.enums;
 
+import com.owz.furry.common.mybatis.EnumTrait;
+
 /**
  * @author kongtong.ouyang created on 2014/9/25, 18:33
  * @version $Id$
  */
-public enum UserStatusType {
+public enum UserStatusType implements EnumTrait {
+    ;
 
 
+    @Override
+    public int getCode() {
+        return 0;
+    }
 }
