d0180e36c931520e0ee699bba1b176db032921b2
kongtong.ouyang <kongtong.ouyang@qunar.com>
2014-09-30 17:39:10 +0800

    initial commit

--- src/main/java/com/owz/algorithm/sort/MergeSort.java
+++ src/main/java/com/owz/algorithm/sort/MergeSort.java
@@ -3,6 +3,8 @@
 */
 package com.owz.algorithm.sort;
 
+import com.google.common.collect.Lists;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,8 +21,8 @@ public class MergeSort {
             return list;
         }
         int half = list.size() / 2;
-        List<E> data1 = new ArrayList<>(half);
-        List<E> data2 = new ArrayList<>(list.size() - half);
+        List<E> data1 = new ArrayList<E>(half);
+        List<E> data2 = new ArrayList<E>(list.size() - half);
         System.arraycopy(list, 0, data1, 0, data1.size());
         System.arraycopy(list, 0, data2, 0, data1.size());
         data1 = mergeSort(data1);
@@ -30,7 +32,7 @@ public class MergeSort {
 
     public static <E extends Comparable<? super E>> List<E> mergeSubSort(List<E> data1, List<E> data2) {
         int i = 0, j = 0, k = 0;
-        List<E> result = new ArrayList<>(data1.size() + data2.size());
+        List<E> result = new ArrayList<E>(data1.size() + data2.size());
         while (i < data1.size() && j < data2.size()) {
             if (data1.get(i).compareTo(data2.get(j)) < 0) {
                 result.set(k++, data1.get(i++));
